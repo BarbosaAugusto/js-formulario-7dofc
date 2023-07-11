12977c48e380fcc529fb002687aceb97b2e195a1
Augusto Barbosa <augusto.barbosa.dev@gmail.com>
2023-07-11 16:00:47 -0300

    initial commit

--- js/valida-idade.js
+++
@@ -1,5 +0,0 @@
-export default function idadeValida(campo) {
-    const dataNascimento = new Date(campo.value);
-    const dataNascimentoFormatada = ((dataNascimento.getDate() + 1)) + "/" + ((dataNascimento.getMonth() + 1)) + "/" + dataNascimento.getFullYear(); 
-    console.log(dataNascimentoFormatada);
-}
