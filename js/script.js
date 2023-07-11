16ae1e0e9d77b0fa2f50887cea8e3f6748eb5883
Augusto Barbosa <augusto.barbosa.dev@gmail.com>
2023-07-11 17:39:51 -0300

    Listando aniversariante na tabela

--- js/script.js
+++ js/script.js
@@ -12,8 +12,43 @@ class Aniversariante {
             this.adicionar(aniversariante)
         }
 
+        this.listaTabela();
         console.log(this.arrayAniversariantes);
 
+    }
+
+    listaTabela() {
+        let tbody = document.getElementById("tbody");
+        tbody.innerText = "";
+        for(let i = 0; i < this.arrayAniversariantes.length; i++) {
+            let tr = tbody.insertRow();
+
+            let td_id = tr.insertCell();
+            let td_nomeAniversariante = tr.insertCell();
+            let td_nascimento = tr.insertCell();
+            let td_acoes = tr.insertCell();
+
+            const nascimento = this.arrayAniversariantes[i].nascimento;
+
+            // Dividir a string de nascimento em partes separadas
+            const partesData = nascimento.split("-");
+            const ano = partesData[0];
+            const mes = partesData[1];
+            const dia = partesData[2];
+
+            // Formatar a data no formato "dd/mm/yyyy"
+            const dataFormatada = `${dia}/${mes}/${ano}`;
+
+            console.log(dataFormatada); // Output: "dd/mm/yyyy"
+
+
+            td_id.innerText = this.arrayAniversariantes[i].id;
+            td_nomeAniversariante.innerText = this.arrayAniversariantes[i].nomeAniversariante;
+            td_nascimento.innerText = dataFormatada;
+
+
+            
+        }
     }
 
     adicionar(aniversariante) {
